96f537ccabcaa86fe829cb1b5b2208b3cb61c0c6
h3rby7 <herbielutz@gmx.net>
2023-10-10 22:27:31 +0200

    added positive tests for extractor

--- controller/enttec/dmxusbpro/extractor_test.go
+++ controller/enttec/dmxusbpro/extractor_test.go
@@ -4,9 +4,68 @@ import (
 	"testing"
 )
 
-// Test extracting from bytes
-func TestExtractorSuccessNoPayload(t *testing.T) {
-	runAndAssertValid(t, 5, []byte{}, []byte{0x7E, 5, 0, 0, 0xE7})
+// raw data is a valid payloadless message, no more, no less
+func TestExtractorSuccessNoPaddingNoPayload(t *testing.T) {
+	label := byte(1)
+	runAndAssertValid(t, label, []byte{}, []byte{0x7E, label, 0, 0, 0xE7})
+}
+
+// raw data has payload.
+func TestExtractorSuccessWithPayload(t *testing.T) {
+	label := byte(2)
+	runAndAssertValid(t, label, []byte{69, 66, 96}, []byte{0x7E, label, 3, 0, 69, 66, 96, 0xE7})
+}
+
+// raw data is padded with rubbish
+func TestExtractorSuccessPadded(t *testing.T) {
+	label := byte(3)
+	runAndAssertValid(t, label, []byte{69, 66, 96}, []byte{0x11, 0x7E, label, 3, 0, 69, 66, 96, 0xE7, 0xEE})
+}
+
+// raw data is padded and has multiple potential starts
+func TestExtractorSuccessMultipleStarts(t *testing.T) {
+	label := byte(4)
+	runAndAssertValid(t, label, []byte{69, 66, 96}, []byte{0x7E, 0x11, 0x7E, label, 3, 0, 69, 66, 96, 0xE7, 0xEE})
+}
+
+// raw data is padded and has multiple potential starts
+func TestExtractorSuccessMultipleEnds(t *testing.T) {
+	label := byte(5)
+	runAndAssertValid(t, label, []byte{69, 66, 96}, []byte{0x11, 0xE7, 0x7E, label, 3, 0, 69, 66, 96, 0xE7, 0xEE, 0xE7})
+}
+
+// raw data is padded and has potential starts inside the data
+func TestExtractorSuccessWithStartsInData(t *testing.T) {
+	label := byte(9)
+	runAndAssertValid(t, label, []byte{69, 0x7E, 96}, []byte{0x11, 0x7E, label, 3, 0, 69, 0x7E, 96, 0xE7, 0xEE})
+}
+
+// raw data is padded and has potential ends inside the data
+func TestExtractorSuccessWithEndsInData(t *testing.T) {
+	label := byte(11)
+	runAndAssertValid(t, label, []byte{69, 0xE7, 96}, []byte{0x11, 0x7E, label, 3, 0, 69, 0xE7, 96, 0xE7, 0xEE})
+}
+
+// raw data is padded and the content would also be a valid message
+// we expect the outer message to be returned
+func TestExtractorSuccessWithDataInData(t *testing.T) {
+	runAndAssertValid(
+		t,
+		9,
+		[]byte{0x7E, 5, 3, 0, 69, 66, 96, 0xE7},
+		[]byte{0x7E, 9, 8, 0, 0x7E, 5, 3, 0, 69, 66, 96, 0xE7, 0xE7},
+	)
+}
+
+// raw data is padded and the content is the beginning of another valid message
+// we expect the first message to be returned
+func TestExtractorSuccessWithDataBeginningInData(t *testing.T) {
+	runAndAssertValid(
+		t,
+		9,
+		[]byte{69, 0x7E, 5, 2, 0},
+		[]byte{0x7E, 9, 5, 0, 69, 0x7E, 5, 2, 0, 0xE7, 0x11, 0xE7},
+	)
 }
 
 func runAndAssertValid(t *testing.T, label byte, payload []byte, input []byte) {
